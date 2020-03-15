f5b639b9d214e7abc5a7c7777a6b9ea231cf29f7
VipulTripathi <vipul.tripathi@agiliad.com>
2020-03-16 00:05:33 +0530

    last change

--- testing.py
+++ testing.py
@@ -1,1 +1,9 @@
-from Learning.jsonFiles import data
+from jsonFiles.data import data
+
+class test:
+    def runtest(self, details):
+        for i in details:
+            print(details[i],' - ', i)
+
+t=test()
+t.runtest(data)
